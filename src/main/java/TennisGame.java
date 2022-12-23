728e90b65f4bf8f2199957d355106d1f2e6f18b9
Jusuf <jusuftopic@yahoo.com>
2022-12-23 08:00:18 +0100

    Git commit

---
+++ src/main/java/TennisGame.java
@@ -0,0 +1,13 @@
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+
+    String getPlayer1Name();
+
+    String getPlayer2Name();
+
+    int getPlayer1Score();
+
+    int getPlayer2Score();
+}
---
+++ src/main/java/TennisGame2.java
@@ -0,0 +1,130 @@
+
+public class TennisGame2 implements TennisGame
+{
+    public int player1Score = 0;
+    public int player2Score = 0;
+    
+    public String P1res = "";
+    public String P2res = "";
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame2(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public String getScore(){
+        String score = "";
+        if (player1Score == player2Score && player1Score < 4)
+        {
+            if (player1Score ==0)
+                score = "Love";
+            if (player1Score ==1)
+                score = "Fifteen";
+            if (player1Score ==2)
+                score = "Thirty";
+            score += "-All";
+        }
+        if (player1Score == player2Score && player1Score >=3)
+            score = "Deuce";
+        
+        if (player1Score > 0 && player2Score ==0)
+        {
+            if (player1Score ==1)
+                P1res = "Fifteen";
+            if (player1Score ==2)
+                P1res = "Thirty";
+            if (player1Score ==3)
+                P1res = "Forty";
+            
+            P2res = "Love";
+            score = P1res + "-" + P2res;
+        }
+        if (player2Score > 0 && player1Score ==0)
+        {
+            if (player2Score ==1)
+                P2res = "Fifteen";
+            if (player2Score ==2)
+                P2res = "Thirty";
+            if (player2Score ==3)
+                P2res = "Forty";
+            
+            P1res = "Love";
+            score = P1res + "-" + P2res;
+        }
+        
+        if (player1Score > player2Score && player1Score < 4)
+        {
+            if (player1Score ==2)
+                P1res="Thirty";
+            if (player1Score ==3)
+                P1res="Forty";
+            if (player2Score ==1)
+                P2res="Fifteen";
+            if (player2Score ==2)
+                P2res="Thirty";
+            score = P1res + "-" + P2res;
+        }
+        if (player2Score > player1Score && player2Score < 4)
+        {
+            if (player2Score ==2)
+                P2res="Thirty";
+            if (player2Score ==3)
+                P2res="Forty";
+            if (player1Score ==1)
+                P1res="Fifteen";
+            if (player1Score ==2)
+                P1res="Thirty";
+            score = P1res + "-" + P2res;
+        }
+        
+        if (player1Score > player2Score && player2Score >= 3)
+        {
+            score = "Advantage player1";
+        }
+        
+        if (player2Score > player1Score && player1Score >= 3)
+        {
+            score = "Advantage player2";
+        }
+        
+        if (player1Score >=4 && player2Score >=0 && (player1Score - player2Score)>=2)
+        {
+            score = "Win for player1";
+        }
+        if (player2Score >=4 && player1Score >=0 && (player2Score - player1Score)>=2)
+        {
+            score = "Win for player2";
+        }
+        return score;
+    }
+    
+
+    public void wonPoint(String player) {
+        if (player == "player1")
+            player1Score++;
+        else
+           player2Score++;
+    }
+
+    @Override
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    @Override
+    public String getPlayer2Name() {
+        return player2Name;
+    }
+
+    @Override
+    public int getPlayer1Score() {
+        return player1Score;
+    }
+
+    @Override
+    public int getPlayer2Score() {
+        return player2Score;
+    }
+}
